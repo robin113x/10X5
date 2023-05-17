f06e532681afc20a7f8f260ac422d3a3a745b0a3
robin113x <robin113x@gmail.com>
2023-05-17 15:06:47 +0530

    10X5

---
+++ src/Array/Q11_stockBuySell.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Q11_stockBuySell {
+   public static int bestSol(int[]arr){
+       int profit=0;
+       for (int i = 1; i < arr.length; i++) {
+           if (arr[i]>arr[i-1])
+               profit+=arr[i]-arr[i-1];
+       }return profit;
+   }
+    public static void main(String[] args) {
+        int arr[] = { 100, 180, 260, 310, 40, 535, 695 };
+        System.out.println(bestSol(arr));
+    }
+}
