20b27fe44507cc98136bfca1e6e5f9c799303ac4
robin113x <robin113x@gmail.com>
2023-05-17 15:06:47 +0530

    10X5

--- src/Array/Q10_MaxDiff.java
+++ src/Array/Q10_MaxDiff.java
@@ -10,8 +10,20 @@ public class Q10_MaxDiff {
         }
         return maxdiff;
     }
+    public static int bestSol(int[]arr){
+        int res=arr[1]-arr[0],minEle=arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            res = Math.max(arr[i]-minEle,res);
+            minEle=Math.min(arr[i],minEle);
+        }
+        
+        return res;
+    }
     public static void main(String[] args) {
        int[]arr = {2,3,10,6,4,8,6,11};
-        System.out.println(naiveSol(arr));
+       int[]arr2={30,10,8,2};
+        System.out.println(naiveSol(arr2));
+        System.out.println(bestSol(arr2));
     }
 }
