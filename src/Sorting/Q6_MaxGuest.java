ff2826b1a3f25f78486ff0c652b1975ab2f48e4a
Robin <robin113x@gmail.com>
2023-05-30 13:45:54 +0530

    10X5

--- src/Sorting/Q6_MaxGuest.java
+++ src/Sorting/Q6_MaxGuest.java
@@ -7,7 +7,7 @@ public class Q6_MaxGuest {
         int n = arr.length;
         Arrays.sort(arr);
         Arrays.sort(dept);
-       i=1,j=0,res=1,curr=1;
+       int i=1,j=0,res=1,curr=1;
        while (i<n && j<n){
            if (arr[i]<=dept[j]){
                curr++;
@@ -21,7 +21,9 @@ public class Q6_MaxGuest {
            return res;
     }
     public static void main(String[] args) {
-
+        int arr[] = { 900, 600, 700};
+        int dep[] = { 1000, 800, 730};
+        System.out.println(maxGuest(arr,dep));
     }
 }
 
