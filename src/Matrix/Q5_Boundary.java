b9b06240015a0cd0cfe8143342a4279bf176bf6a
Robin <robin113x@gmail.com>
2023-06-01 16:44:35 +0530

    10X5

--- src/Matrix/Q5_Boundary.java
+++ src/Matrix/Q5_Boundary.java
@@ -1,12 +1,16 @@
 import java.util.*;
 class Q5_Boundary{
+	public static void sol(int[][]mat){
+		int row=mat.length;
+		int col = mat[0].length;
+		System.out.println(row+" "+col);
+	}
 
 
-	
 	private static void disp(int[][]mat){
 		System.out.println("**********************");
 		for(int i=0;i<mat.length;i++){
-			for(int j=0;j<mat.length;j++){
+			for(int j=0;j<mat[i].length;j++){
 				System.out.print(mat[i][j]+" ");
 			}
 			System.out.println();
@@ -16,9 +20,10 @@ class Q5_Boundary{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][]mat = new int[n][n];	
+		int m = sc.nextInt();
+		int[][]mat = new int[n][m];	
 		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
+			for(int j=0;j<m;j++){
 				mat[i][j]=sc.nextInt();
 			}
 		}
--- src/Matrix/syntax.java
+++ src/Matrix/syntax.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class syntax{
 
-	pu
+
 
 
 	private static void disp(int[][]mat){
 		System.out.println("**********************");
 		for(int i=0;i<mat.length;i++){
-			for(int j=0;j<mat.length;j++){
+			for(int j=0;j<mat[i].length;j++){
 				System.out.print(mat[i][j]+" ");
 			}
 			System.out.println();
@@ -16,10 +16,11 @@ class syntax{
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[][]mat = new int[n][n];	
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		int[][]mat = new int[row][col];	
+		for(int i=0;i<row;i++){
+			for(int j=0;j<col;j++){
 				mat[i][j]=sc.nextInt();
 			}
 		}
