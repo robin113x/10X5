d295adac88c0f002d7e313b8e0cf88816228f386
robin113x <robin113x@gmail.com>
2023-06-02 21:36:46 +0530

    10X5

--- src/HashingPro/Q1_CountDist.java
+++ src/HashingPro/Q1_CountDist.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
-class synatax{
+class Q1_CountDist{
+	public static int sol(int[]arr){
+		Set<Integer> s = new HashSet<>();
+		for(int i=0;i<arr.length;i++){
+			s.add(arr[i]);
+		}
+		return s.size();
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -9,6 +16,8 @@ class synatax{
 			arr[i]=sc.nextInt();
 		}
 		System.out.println(Arrays.toString(arr));
+		System.out.println(sol(arr));
+
 		
 	}
 }
